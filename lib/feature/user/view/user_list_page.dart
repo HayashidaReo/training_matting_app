1d39ba788225b600e80d40855ae4a2c8bfea7493
hayashidareo <reo0524h@gmail.com>
2025-03-10 14:28:17 +0900

    user検索のUIを調整

--- lib/feature/user/view/user_list_page.dart
+++ lib/feature/user/view/user_list_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:matching_app/config/utils/decoration/text_field_decoration.dart';
-import 'package:matching_app/feature/component/single_line_text_form_field.dart';
 import 'package:matching_app/feature/user/controller/user_controller.dart';
 import 'package:matching_app/feature/user/data_model/userdata.dart';
 
@@ -18,102 +17,109 @@ class UserListPage extends HookConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: TextField(
-          controller: searchTextController,
-          decoration: textFieldDecoration('ユーザー検索'),
-          onChanged: (text) {
-            searchText.value = text;
-          },
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 4),
-            child: IconButton(
-              icon: const Icon(Icons.search),
-              iconSize: 24,
-              onPressed: () {
-                searchText.value = searchTextController.text;
+        title: Stack(
+          children: [
+            TextField(
+              controller: searchTextController,
+              decoration: textFieldDecoration('ユーザー検索'),
+              onChanged: (text) {
+                searchText.value = text;
               },
             ),
-          ),
-        ],
-      ),
-      body: ref
-          .watch(
-            watchForwardMatchingWithQueryTextUsersControllerProvider(
-              searchText.value,
+            Positioned(
+              right: 5,
+              top: 10,
+              bottom: 10,
+              child: IconButton(
+                icon: const Icon(Icons.search),
+                iconSize: 24,
+                onPressed: () {
+                  FocusScope.of(context).unfocus();
+                },
+              ),
             ),
-          )
-          .when(
-            error: (error, _) {
-              return const Center(child: Text('エラーが発生しました'));
-            },
-            loading: () {
-              return const Center(child: CircularProgressIndicator());
-            },
-            data: (List<UserData> userDataList) {
-              return ListView.separated(
-                itemCount: userDataList.length,
-                separatorBuilder: (context, index) {
-                  return Divider();
-                },
-                itemBuilder: (context, index) {
-                  final UserData userData = userDataList[index];
-                  return ListTile(
-                    leading:
-                        (userData.iconImageUrl.isEmpty)
-                            ? ClipOval(
-                              child: Image.asset(
-                                'assets/images/default_user_icon.png',
-                                width: 60,
-                                height: 60,
-                                fit: BoxFit.cover,
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ref
+            .watch(
+              watchForwardMatchingWithQueryTextUsersControllerProvider(
+                searchText.value,
+              ),
+            )
+            .when(
+              error: (error, _) {
+                return const Center(child: Text('エラーが発生しました'));
+              },
+              loading: () {
+                return const Center(child: CircularProgressIndicator());
+              },
+              data: (List<UserData> userDataList) {
+                return ListView.separated(
+                  itemCount: userDataList.length,
+                  separatorBuilder: (context, index) {
+                    return Divider();
+                  },
+                  itemBuilder: (context, index) {
+                    final UserData userData = userDataList[index];
+                    return ListTile(
+                      leading:
+                          (userData.iconImageUrl.isEmpty)
+                              ? ClipOval(
+                                child: Image.asset(
+                                  'assets/images/default_user_icon.png',
+                                  width: 60,
+                                  height: 60,
+                                  fit: BoxFit.cover,
+                                ),
+                              )
+                              : CachedNetworkImage(
+                                imageUrl: userData.iconImageUrl,
+                                imageBuilder:
+                                    (context, imageProvider) => ClipOval(
+                                      child: Image(
+                                        image: imageProvider,
+                                        width: 60,
+                                        height: 60,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                progressIndicatorBuilder: (
+                                  context,
+                                  url,
+                                  downloadProgress,
+                                ) {
+                                  return SizedBox(
+                                    width: 60,
+                                    height: 60,
+                                    child: Center(
+                                      child: CircularProgressIndicator(
+                                        value: downloadProgress.progress,
+                                      ),
+                                    ),
+                                  );
+                                },
+                                errorWidget: (context, url, error) {
+                                  return SizedBox(
+                                    width: 60,
+                                    height: 60,
+                                    child: Icon(
+                                      Icons.image_not_supported_rounded,
+                                    ),
+                                  );
+                                },
                               ),
-                            )
-                            : CachedNetworkImage(
-                              imageUrl: userData.iconImageUrl,
-                              imageBuilder:
-                                  (context, imageProvider) => ClipOval(
-                                    child: Image(
-                                      image: imageProvider,
-                                      width: 60,
-                                      height: 60,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                              progressIndicatorBuilder: (
-                                context,
-                                url,
-                                downloadProgress,
-                              ) {
-                                return SizedBox(
-                                  width: 60,
-                                  height: 60,
-                                  child: Center(
-                                    child: CircularProgressIndicator(
-                                      value: downloadProgress.progress,
-                                    ),
-                                  ),
-                                );
-                              },
-                              errorWidget: (context, url, error) {
-                                return SizedBox(
-                                  width: 60,
-                                  height: 60,
-                                  child: Icon(
-                                    Icons.image_not_supported_rounded,
-                                  ),
-                                );
-                              },
-                            ),
-                    title: Text(userData.userName),
+                      title: Text(userData.userName),
 
-                    onTap: () {},
-                  );
-                },
-              );
-            },
-          ),
+                      onTap: () {},
+                    );
+                  },
+                );
+              },
+            ),
+      ),
     );
   }
 }

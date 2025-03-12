f1b6ef92b636264265d2b22770937997b97918eb
hayashidareo <reo0524h@gmail.com>
2025-03-12 11:40:23 +0900

    相手側のメッセージも表示を整形

--- lib/feature/talk/view/talk_room_page.dart
+++ lib/feature/talk/view/talk_room_page.dart
@@ -14,6 +14,7 @@ import 'package:matching_app/feature/talk/controller/talk_history_controller.dar
 import 'package:matching_app/feature/talk/model/talk_history.dart';
 import 'package:matching_app/feature/user/controller/storage_controller.dart';
 import 'package:matching_app/feature/user/controller/user_controller.dart';
+import 'package:matching_app/feature/user/model/userdata.dart';
 import 'package:matching_app/function/get_image_from_gallery.dart';
 import 'package:uuid/uuid.dart';
 
@@ -85,45 +86,9 @@ class TalkRoomPage extends HookConsumerWidget {
                                             ? MyMassageHistoryTile(
                                               talkHistoryData: talkHistoryData,
                                             )
-                                            : Expanded(
-                                              child: Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment.start,
-                                                children: [
-                                                  IconImage(
-                                                    iconImageUrl:
-                                                        targetUserData
-                                                            .iconImageUrl,
-                                                    size: 36,
-                                                  ),
-                                                  Container(
-                                                    padding:
-                                                        const EdgeInsets.all(
-                                                          6.0,
-                                                        ),
-                                                    margin:
-                                                        const EdgeInsets.all(
-                                                          6.0,
-                                                        ),
-                                                    decoration: BoxDecoration(
-                                                      color:
-                                                          defaultColors
-                                                              .unavailableFrontGreyColor,
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                            12.0,
-                                                          ),
-                                                    ),
-                                                    child: Text(
-                                                      talkHistoryDataList[index]
-                                                          .message,
-                                                      style: TextStyle(
-                                                        color: Colors.white,
-                                                      ),
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
+                                            : InterlocutorMassageHistoryTile(
+                                              talkHistoryData: talkHistoryData,
+                                              targetUserData: targetUserData,
                                             ),
                                       ],
                                     );
@@ -281,6 +246,64 @@ class TalkRoomPage extends HookConsumerWidget {
     messageTextController.clear();
     uploadedImageFile.value = null;
     return;
+  }
+}
+
+class InterlocutorMassageHistoryTile extends StatelessWidget {
+  const InterlocutorMassageHistoryTile({
+    super.key,
+    required this.talkHistoryData,
+    required this.targetUserData,
+  });
+
+  final TalkHistory talkHistoryData;
+  final UserData targetUserData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          IconImage(iconImageUrl: targetUserData.iconImageUrl, size: 36),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              (talkHistoryData.imageUrl.isNotEmpty)
+                  ? Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 0.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: AutoScaledImage(
+                        imageUrl: talkHistoryData.imageUrl,
+                        targetMinSide: 170,
+                        fit: BoxFit.contain,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  )
+                  : SizedBox.shrink(),
+              (talkHistoryData.message.isNotEmpty)
+                  ? Container(
+                    constraints: const BoxConstraints(maxWidth: 300),
+                    padding: const EdgeInsets.all(6.0),
+                    margin: const EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      color: defaultColors.unavailableFrontGreyColor,
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Text(
+                      talkHistoryData.message,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                  : SizedBox.shrink(),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
